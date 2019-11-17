0fed8f74331ffdea539cec4a504ba99dbd34b6f0
ryujisuda <cherryhee_@naver.com>
2019-11-18 01:04:52 +0900

    Drawer Main에 각 기능들 오류 수정

--- app/src/main/java/com/raonhaze_pbl/myapplication/Main2Activity.java
+++ app/src/main/java/com/raonhaze_pbl/myapplication/Main2Activity.java
@@ -40,15 +40,15 @@ import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 public class Main2Activity extends AppCompatActivity {
-
-
+    private Button qr_button,notice_button,map_button;
+    private IntentIntegrator qrScan;
+    private String userID;
     Intent intent;
     private AppBarConfiguration mAppBarConfiguration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -75,9 +75,98 @@ public class Main2Activity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
 
         intent =getIntent();
-        TextView hi = (TextView)navigationView.getHeaderView(0).findViewById(R.id.user_place);
-        String userID=intent.getStringExtra("userID");
+        TextView hi = (TextView)navigationView.getHeaderView(0).findViewById(R.id.user_place);//다른 xml 파일에 있는 TextView 건들기.
+        userID=intent.getStringExtra("userID");
         hi.setText(userID+"님, 환영합니다!");
+        qr_button =findViewById(R.id.Button_click);
+        qrScan = new IntentIntegrator(this);
+        // qrScan.setCaptureActivity(MainActivity.class);
+
+        qr_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                qrScan.setOrientationLocked(false);
+                qrScan.initiateScan();
+                //scan option
+                qrScan.setPrompt("Scanning...");
+                //qrScan.setOrientationLocked(false);
+                qrScan.initiateScan();
+            }
+        });
+        //notice 버튼을 누르면 저 홈페이지로 가게..
+        notice_button =findViewById(R.id.Button_notice);
+        notice_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.yeonsu.go.kr/main/part/clean/house.asp"));
+                startActivity(webIntent);
+            }
+        });
+        map_button = findViewById(R.id.Button_map);
+        map_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent webIntent = new Intent(getApplicationContext(), TmapActivity.class);
+                startActivity(webIntent);
+            }
+        });
+    }
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //qrcode 가 없으면
+            if (result.getContents() == null) {
+            } else {
+                //qrcode 결과가 있으면
+                try {
+                    //data를 json으로 변환
+                    JSONObject obj = new JSONObject(result.getContents());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                    Intent intent =  getIntent();
+
+                    final String userBarcode = result.getContents();
+                    Response.Listener<String> responseListener = new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            //여기서 부터 json object를 활용한다. json object는 string 형태.. 담아서 보내는 운반체라고 생각하면 된다.
+                            try {
+
+                                JSONObject jsonObject = new JSONObject(response);
+                                Boolean success = jsonObject.getBoolean("success");//success를 가지고 와야해 true냐?->성공 false냐?->실패
+                                if(success)
+                                {
+                                    Toast.makeText(getApplicationContext(),"전송이 완료되었습니다.",Toast.LENGTH_SHORT).show();
+
+                                }
+                                else
+                                {
+                                    Toast.makeText(getApplicationContext(),"전송이 실패하였습니다.",Toast.LENGTH_SHORT).show();
+                                    return;//넘어가면 안되니까! return 해버리기
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    };
+                    BarcodeRequest barcodeRequest = new BarcodeRequest(userID,userBarcode,responseListener);
+                    RequestQueue queue = Volley.newRequestQueue(Main2Activity.this);
+                    queue.add(barcodeRequest);
+
+
+                }
+            }
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+
     }
 
 
